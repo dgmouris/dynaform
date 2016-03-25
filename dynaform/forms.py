b14776fc105edcbe10dcb5a3c9ce82a7eb283ecf
Dan Mouris <dgmouris@gmail.com>
2016-03-25 17:20:01 -0600

    ideas added to the forms and views file to add to the environment to make nesting a possibility

--- dynaform/forms.py
+++ dynaform/forms.py
@@ -17,8 +17,7 @@ class QuestionsForm(forms.Form):
 		self.questionaire=kwargs.pop('questionaire')
 		super(QuestionsForm,self).__init__(*args,**kwargs)
 		
-		#questions = self.get_questionaire()
-
+		#these are the questions that you can 
 		questionaire = LegalQuestionaire.objects.get(pk=self.questionaire)
 		questions= Questions.objects.filter(questionaire=questionaire)
 		for question in questions:
@@ -37,3 +36,37 @@ class LegalTemplatesUploadForm(forms.Form):
         label='Select a file',
         help_text='max. 42 megabytes'
     )
+
+class QuestionsFormNest(forms.Form):
+
+
+	#this is going to pop out the questionaire id created in the view
+	def __init__(self,*args,**kwargs):
+		self.questionaire=kwargs.pop('questionaire')
+		super(QuestionsForm,self).__init__(*args,**kwargs)
+		
+		#to create the nestable asset you need to create the three tags below.
+		'''
+		get the status of this part of the questionaire from some part of the models
+
+		//this is going to have to the class name of the parent so that you can get the value from the div
+		self.fields['parent_name'] 
+		self.fields['parent_value'] the iterator value so that you can tie the form information to it.
+		self.fields['child_name'] (this is going to default to nothing, "None" is probably the best option)
+		self.fields['form_name'] (this is going to be the default name)
+		//the form name and the parent name need to match.
+		//these fields need to all be hidden and rendered in the template.
+		'''
+		#this creates the questions based on the questionaire number
+		questionaire = LegalQuestionaire.objects.get(pk=self.questionaire)
+		questions= Questions.objects.filter(questionaire=questionaire)
+		for question in questions:
+			#self.fields['custom_%s' % question.label] = forms.CharField(max_length=255) 
+			self.fields['custom_%s' % question.label] = FIELD_TYPES[question.field_type]
+
+	#this is going to get the questions from the database
+
+	def get_questionaire(self):
+		questionaire = LegalQuestionaire.objects.get(pk=self.questionsionaire)
+		questions= Questions.objects.filter(questionaire=questionaire)
+		return questions
--- dynaform/urls.py
+++ dynaform/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^(?P<lq_id>\d+)/$', views.questionaire_view, name='questionaire'),
     url(r'^formset/(?P<lq_id>\d+)/$', views.formset_view, name='formset'),
+    url(r'^multi-formset/(?P<lq_id>\d+)/$', views.multi_formset_view, name='multi_formset'),
     url(r'^upload-templates/$',views.legal_template_upload_view,name="legal templates")
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
